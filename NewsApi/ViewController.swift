a91dc89af28469de24751fbf48132f00de8288de
Alex <sanya1444@gmail.com>
2020-11-20 10:50:44 +0200

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by Oleksandr Karpenko on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
